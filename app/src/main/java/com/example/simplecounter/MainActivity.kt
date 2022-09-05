fcf50c72cfcd6ea726fb3a0211411b3aab0d988b
Maize Booker <maizebooker@Maizes-MacBook-Pro.local>
2022-09-05 18:45:56 -0400

    first commit

---
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.simplecounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<android.widget.TextView>(R.id.textView)
+        val button = findViewById<android.widget.Button>(R.id.button)
+
+        button.setOnClickListener {
+//            android.widget.Toast.makeText(it.context, "Clicked Button!", android.widget.Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+        }
+    }
+}
